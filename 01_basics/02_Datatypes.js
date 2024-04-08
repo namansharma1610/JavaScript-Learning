11add5438304a5d66544041e0bcb72091e4b6d6c
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-08 11:09:01 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let name ="Naman"
+let age=22
+let isloggedIn=false
+let string; 
+
+// number
+// bigint
+//string => " "
+//boolean => true/false
+//null =>standalone value
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
