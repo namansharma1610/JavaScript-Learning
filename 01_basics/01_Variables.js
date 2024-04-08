9effe3b420124ccd98627a7213f498bd2fbc5cdb
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-08 10:59:01 +0000

    Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId=1254689
+let accountEmail="Naman@gmail.com"
+var accountPassword=1258963
+accountCity="Delhi"
+
+// accountId=14584 //Not Allowed 
+
+/* 
+Prefer not to use Var
+because of issues in functional scope and block scope 
+*/
+accountEmail="Jack@gmail.com"
+accountPassword=15896
+accountCity="Jaipur"
+console.table([accountEmail,accountId,accountPassword,accountCity])
