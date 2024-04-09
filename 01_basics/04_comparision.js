5629ff1e7e1081322fbee69d42ca11f3ec296386
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-09 03:46:08 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// This type of conversion is always confusing 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// We always write clean code which is easy to readable 
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+console.log("2"=== 2);
+
+
+
+
+
