500e9f215e2299c0f927ecbda6fc5bc5ac64a060
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-09 13:54:35 +0000

    Strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,14 @@
+const name="Naman"
+const repoCount=50
+// console.log(name + repoCount + "Value") // we dont use this traditional style of writing 
+// console.log(`Hello My Name is ${name} and My repo count is ${repoCount}`);
+const gameName= new String("Naman Sharma")
+console.log(gameName[0]) // N
+console.log(gameName.length) // 12
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'))
+console.log(gameName); 
+const newString= gameName.substring(0,5)
+console.log(newString);
