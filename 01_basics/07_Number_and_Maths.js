c654d0977ac7c1fe7b10164a3b946e1be4974f7f
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-10 08:43:50 +0000

    Numbers and Masths

--- 01_basics/07_Number_and_Maths.js
+++ 01_basics/07_Number_and_Maths.js
@@ -31,9 +31,14 @@
 // console.log(Math.min(4,8,9,2,1)); // 1
 // console.log(Math.max(4,8,9,2,1)); // 9
 
-console.log(Math.random());
-console.log((Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min )
 
 
 
