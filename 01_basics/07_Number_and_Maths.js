07a2cfc1cf50c0543478fe419f59b1dbe9a38ae2
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-10 06:11:11 +0000

    Numbers and Maths

--- 01_basics/07_Number_and_Maths.js
+++ 01_basics/07_Number_and_Maths.js
@@ -1,12 +1,42 @@
+//***************Numbers*********************//
 // const score=400
 // console.log(score);
 // console.log(typeof score);
 
-const balance= new Number(100)
-console.log(balance); // [Number: 100]
+// const balance= new Number(100)
+// console.log(balance); // [Number: 100]
 
-console.log(typeof balance); // Object
-console.log(typeof (balance.toString())); // string
-console.log(typeof balance) // object
-console.log(balance.toString().length); // length = 3
+// console.log(typeof balance); // Object
+// console.log(typeof (balance.toString())); // string
+// console.log(typeof balance) // object
+// console.log(balance.toString().length); // length = 3
+
+// const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//*****************************Maths**************************//
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+// /*absolute means negative number converted to postive number*/ 
+
+// console.log(Math.round(4.3)); //round of = 4
+// console.log(Math.round(4.6)); //round of = 5
+// console.log(Math.ceil(4.1)); //celing high value always
+// console.log(Math.floor(4.9)); //floor high  value always
+// console.log(Math.min(4,8,9,2,1)); // 1
+// console.log(Math.max(4,8,9,2,1)); // 9
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+
+
 
