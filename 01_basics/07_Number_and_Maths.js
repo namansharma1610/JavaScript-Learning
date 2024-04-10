b633d0acd0679e2fde4da06fdc7c2a4566fad4e5
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-10 05:29:21 +0000

    Numbers and Maths

---
+++ 01_basics/07_Number_and_Maths.js
@@ -0,0 +1,12 @@
+// const score=400
+// console.log(score);
+// console.log(typeof score);
+
+const balance= new Number(100)
+console.log(balance); // [Number: 100]
+
+console.log(typeof balance); // Object
+console.log(typeof (balance.toString())); // string
+console.log(typeof balance) // object
+console.log(balance.toString().length); // length = 3
+
