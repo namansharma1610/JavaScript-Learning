0db446c58fbfe2beee4923b12850d88557d44542
namansharma1610 <72177720+namansharma1610@users.noreply.github.com>
2024-04-10 03:07:13 +0000

    strings

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -1,14 +1,26 @@
-const name="Naman"
-const repoCount=50
+// const name="Naman"
+// const repoCount=50
 // console.log(name + repoCount + "Value") // we dont use this traditional style of writing 
 // console.log(`Hello My Name is ${name} and My repo count is ${repoCount}`);
-const gameName= new String("Naman Sharma")
-console.log(gameName[0]) // N
-console.log(gameName.length) // 12
-console.log(gameName.toLowerCase())
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('a'))
-console.log(gameName); 
-const newString= gameName.substring(0,5)
-console.log(newString);
+const gameName= new String("Naman-Sharma-gmail-com")
+// console.log(gameName[0]) // N
+// console.log(gameName.length) // 12
+// console.log(gameName.toLowerCase())
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'))
+// console.log(gameName); 
+// const newString= gameName.substring(0,5)
+// console.log(newString);
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+const Name= "     Naman Sharma    "
+console.log(Name.trim());
+console.log(Name.trimEnd());
+console.log(Name.trimStart());
+const url = "https//NamanYt@gmail.com"
+console.log(url.replace("@","-"))
+console.log(url.includes("Naman"));
+console.log(url.includes("SundarVera"));
+console.log(gameName.split("-"))
+
